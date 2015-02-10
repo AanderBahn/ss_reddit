dee2ce2445706c63e19e021682ba629647fa7507
Paul Gonzalez <pjg5005@gmail.com>
2015-02-10 14:55:26 -0600

    Adds comments in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.destroy_all
 100.times do |n|
@@ -29,7 +24,7 @@ puts "#{Post.all.count} posts have been created"
 posts = Post.all
 
 Vote.destroy_all
-500.times do
+1000.times do
   Vote.create!( user_id: users.pluck(:id).sample,
                 votable_id: posts.pluck(:id).sample,
                 votable_type: "Post"
@@ -37,6 +32,15 @@ Vote.destroy_all
 end
 puts "#{Vote.all.count} votes have been created"
 
+Comment.destroy_all
+500.times do
+  Comment.create!(  user_id: users.pluck(:id).sample,
+                    post_id: posts.pluck(:id).sample,
+                    text: Faker::Lorem.sentence(6)
+                    )
+end
+
+puts "#{Comment.all.count} comments have been created"
 
 
 
